e577f07bc1e13afff784f65594c9371040006687
yiczhu_qle <yiczhu_qle@amd.com>
2026-08-25 07:36:54 -0500

    Optimize DCP empty shard masking

--- vllm/model_executor/layers/attention/mla_attention.py
+++ vllm/model_executor/layers/attention/mla_attention.py
@@ -1013,6 +1013,11 @@ class MLAAttention(nn.Module, AttentionLayerBase):
                     lse,
                     seq_lens=seq_lens,
                     query_start_loc=query_start_loc,
+                    skip_empty_shard_mask=(
+                        decode_metadata.no_empty_dcp_shards
+                        if decode_metadata is not None
+                        else False
+                    ),
                 )
                 if self.use_pcp:
                     attn_out = finalize_mla_pcp_decode(attn_out, self.num_heads)
@@ -1530,6 +1535,7 @@ class MLACommonDecodeMetadata:
     block_table: torch.Tensor
     seq_lens: torch.Tensor
     dcp_tot_seq_lens: torch.Tensor | None
+    no_empty_dcp_shards: bool = False
 
 
 D = TypeVar("D", bound=MLACommonDecodeMetadata)
@@ -2392,6 +2398,7 @@ class MLACommonMetadataBuilder(AttentionMetadataBuilder[M]):
         decode_metadata = None
         if num_decodes > 0:
             dcp_tot_seq_lens_device = None
+            no_empty_dcp_shards = False
             if self.dcp_world_size > 1:
                 assert seq_lens is not None, (
                     "MLA DCP decode requires seq_lens on CommonAttentionMetadata"
@@ -2406,6 +2413,25 @@ class MLACommonMetadataBuilder(AttentionMetadataBuilder[M]):
                     )
                 dcp_tot_seq_lens_device = seq_lens[:num_decodes]
                 seq_lens = dcp_local_seq_lens
+                dcp_local_seq_lens_cpu = common_attn_metadata.dcp_local_seq_lens_cpu
+                if dcp_local_seq_lens_cpu is not None:
+                    decode_query_start_loc_cpu = query_start_loc_cpu[: num_decodes + 1]
+                    query_lens_cpu = (
+                        decode_query_start_loc_cpu[1:] - decode_query_start_loc_cpu[:-1]
+                    )
+                    active_reqs = query_lens_cpu > 0
+                    no_padding_rows = (
+                        int(decode_query_start_loc_cpu[-1]) == num_decode_tokens
+                    )
+                    no_empty_dcp_shards = (
+                        no_padding_rows
+                        and bool(torch.any(active_reqs))
+                        and bool(
+                            torch.all(
+                                dcp_local_seq_lens_cpu[:num_decodes][active_reqs] > 0
+                            )
+                        )
+                    )
 
                 # After DCP distribution, the maximum number of tokens for any rank is
                 # ceil(L / (N * I)) * I, where L is max_seq_len, N is dcp_world_size,
@@ -2426,6 +2452,7 @@ class MLACommonMetadataBuilder(AttentionMetadataBuilder[M]):
                 num_decode_tokens=num_decode_tokens,
                 dcp_tot_seq_lens_device=dcp_tot_seq_lens_device,
             )
+            decode_metadata.no_empty_dcp_shards = no_empty_dcp_shards
 
         attn_metadata = self.metadata_cls(
             num_reqs=common_attn_metadata.num_reqs,
